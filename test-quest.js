34e8dd00b3e9676c8cab337b5582dd5f29f29d06
Eugene <omelchenko1595@gmail.com>
2022-12-28 17:02:24 +0200

    Finish 28/12

---
+++ test-quest.js
@@ -0,0 +1,71 @@
+"use strict"
+
+//1
+let a = "1" && true || null && 1;
+console.log(a);                      // will be True;
+
+//2 - without Q
+
+//3 
+var a1 = 10;
+var b1 = 15;
+
+console.log((a1 % 2 || b1 % 3) ? 'Try again' : 'OK');  // will be OK
+
+//4 - will be D
+
+//5 
+let counter = 1;
+console.log(counter++); // will be 1 becouse of postfix??????????????
+
+//6 - without Q
+
+//7  - will be A
+
+//8 
+
+console.log(employeed);
+var employeed = '1900';  // will be undefined
+
+//9 - wothout Q
+
+//10 - without Q
+
+//11 -  will be C
+
+//12 
+
+console.log(6 / '3'); // will be 2 
+
+//13 - without Q
+
+//14 will be panding, fillfuled, reject A
+
+//15 - without Q
+
+//16 
+
+console.log(true + false); // will be 1
+
+//17 - will be none A
+
+//18 - ???? i think it will be D
+
+//19 
+
+console.log(undefined || null || 0); // will be 0  C
+
+//20 - ?? i think пустому новому обьекту С
+
+//21 - without Q
+
+//22 - without Q
+
+//23 - псевдоклас hover
+
+//24 - without Q
+
+//25  поднятие вверх по цепочке А;
+
+
+
