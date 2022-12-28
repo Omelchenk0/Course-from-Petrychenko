7a3c0c08a804e712a23fe301b20a353a309d72ff
Eugene <omelchenko1595@gmail.com>
2022-12-28 12:43:23 +0200

    Start 28.12.2022

---
+++ js/22-цикли.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// Цикл "While"
+
+let num = 50;
+
+while (num <= 55) {
+   console.log(num);
+   num++;
+};
+
+// цикл "do while"
+
+let numm = 50;
+
+do {
+   console.log(numm);
+   numm++;
+}
+while (numm < 55);
+
+// цикл "for" - найпопулярніший
+
+
+let nuum = 50;
+
+for (let i = 1; i < 8; i++){
+   console.log(nuum);
+   nuum++;
+}
