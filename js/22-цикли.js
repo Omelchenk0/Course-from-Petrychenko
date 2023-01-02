9acb9aa160f7e5ae5cc21af41e21535db0d0b453
Eugene <omelchenko1595@gmail.com>
2023-01-02 15:56:53 +0200

    I`m done with task #24

--- js/22-цикли.js
+++ js/22-цикли.js
@@ -7,7 +7,8 @@ let num = 50;
 while (num <= 55) {
    console.log(num);
    num++;
-};
+}; 
+//виведетьться від 50 до 55 включно;
 
 // цикл "do while"
 
@@ -17,7 +18,8 @@ do {
    console.log(numm);
    numm++;
 }
-while (numm < 55);
+while (numm < 55); 
+//виведеться від 50 до 54 включно;
 
 // цикл "for" - найпопулярніший
 
@@ -27,4 +29,5 @@ let nuum = 50;
 for (let i = 1; i < 8; i++){
    console.log(nuum);
    nuum++;
-}
+}            
+//виведеться від 50 до 56  включно        
