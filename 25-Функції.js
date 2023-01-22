b9d7584057351788279cd30a841df85c643114d0
Eugene Omelchenko <e.omelchenko15@gmail.com>
2023-01-22 13:51:32 +0200

    22.01.2023

--- 25-Функції.js
+++ 25-Функції.js
@@ -1,1 +1,28 @@
 "use strict"
+
+function showFirstMessage() {
+   console.log("Hello World");
+}
+
+showFirstMessage();
+
+//or we can write like:
+
+function showFirstMessage(text) {
+   console.log(text);
+   let num = 20; //створили локальну змінну через let/const яка існує тільки всередині функції
+               //і ми не можемо викликати її за межами цієї функції
+               //але ми можемо використовувати змінну яка записана за межами функції
+}
+
+showFirstMessage("Hello World");
+
+// калькулятор
+
+function calc(a, b) {
+   return (a + b);
+}
+
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
