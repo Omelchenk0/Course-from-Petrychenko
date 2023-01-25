c0016d056e7b77a44ff47dbdcb7941f855b4b58e
Eugene Omelchenko <e.omelchenko15@gmail.com>
2023-01-25 11:01:38 +0200

    25.01 START

---
+++ 6-Упражнения по написанию кода Функции.js
@@ -0,0 +1,21 @@
+// // Место для первой задачи
+// function sayHello(name) {
+//    return `Привіт, ${name}!`;
+// }
+
+// sayHello('Eugene');
+
+function sayHello(Eugene) {
+   return `Привет, ${Eugene}!`;
+}
+
+sayHello('Alex');
+// Место для второй задачи
+function returnNeighboringNumbers() {
+
+}
+
+// Место для третьей задачи
+function getMathResult() {
+
+}
